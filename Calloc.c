c54159fc1c4a4d9a7efa588fc8823c48a7f96391
Lucky Tarak <118117144+luckycomputer01@users.noreply.github.com>
2022-11-23 21:37:45 +0530

    Add files via upload

---
+++ Calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+	int i,n;
+	printf("How many element you want to enter: ");
+	scanf("%d",&n);
+	ptr=(int *)calloc(n, sizeof(int));
+	for(i=0;i<n;i++)
+	{
+	  scanf("%d",&ptr[i]);
+  	}
+ 	printf("Your array element\n");
+	for(i=0;i<n;i++)
+	{
+	  printf("%d\n",ptr[i]);
+	}
+	free(ptr);
+
+    return 0;
+}
